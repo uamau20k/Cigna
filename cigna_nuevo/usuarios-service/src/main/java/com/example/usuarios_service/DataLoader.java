4804b1eb3396293a184b36721f14b94a26905230
uamau20k <ma.ahumadas@duocuc.cl>
2026-06-29 19:50:00 -0400

    se arreglo el formato para numeros en dataloader

--- cigna_nuevo/usuarios-service/src/main/java/com/example/usuarios_service/DataLoader.java
+++ cigna_nuevo/usuarios-service/src/main/java/com/example/usuarios_service/DataLoader.java
@@ -3,6 +3,9 @@ package com.example.usuarios_service;
 import com.example.usuarios_service.model.Usuario;
 import com.example.usuarios_service.repository.UsuarioRepository;
 import net.datafaker.Faker;
+
+import java.util.Random;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,16 +18,19 @@ public class DataLoader implements CommandLineRunner {
         this.usuarioRepository = usuarioRepository;
     }
 
-    @Override
+   @Override
     public void run(String... args) throws Exception {
         Faker faker = new Faker(java.util.Locale.forLanguageTag("es"));
+        Random random = new Random();
+
         for (int i = 0; i < 5; i++) {
             if (usuarioRepository.count() >= 10) break;
             Usuario u = new Usuario();
             u.setNombre(faker.name().firstName());
             u.setApellido(faker.name().lastName());
             u.setCorreo(faker.internet().emailAddress());
-            u.setTelefono(faker.phoneNumber().phoneNumber());
+            String numero = String.format("+56 9 %04d %04d", random.nextInt(9000) + 1000, random.nextInt(9000) + 1000);
+            u.setTelefono(numero);
             u.setActivo(true);
             usuarioRepository.save(u);
         }
